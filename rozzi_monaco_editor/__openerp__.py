b67a80d2c319d4324bc41cdea7ba627a49419552
开阖软件 Jeff WANG <jeff@osbzr.com>
2018-03-05 16:32:25 +0800

    文本框支持Monaco编辑器

---
+++ rozzi_monaco_editor/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+
+
+{
+    'name': 'monaco编辑器',
+    'version': '1.0.0',
+    'category': 'Web',
+    'author': "Jerry Luo(410881525@qq.com)",
+    'website': 'http://www.csfinox.cn',
+    'license': 'AGPL-3',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'view.xml',
+
+    ],
+    'qweb': [
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
