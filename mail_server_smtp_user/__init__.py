d8218b64c2c9eb1660734b1980a7cb1917f7796f
开阖软件 Jeff WANG <jeff@osbzr.com>
2018-03-15 12:22:06 +0800

    使用发件服务器的邮箱作为发件人

---
+++ mail_server_smtp_user/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 wangbuke <wangbuke@gmail.com>
+# Copyright 2017 Jarvis <jarvis@odoomod.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+import models
