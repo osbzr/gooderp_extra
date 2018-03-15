4dbe54875be049b132d93235fe88bcccc85c2652
flora <flora@osbzr.com>
2018-03-15 17:42:59 +0800

    在报表模板中通过复制按钮自动产生Qweb视图

---
+++ base_report_auto_create_qweb/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Authors: See README.RST for Contributors
+# Copyright 2015-2017
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Report qweb auto generation",
+    "version": "10.0.1.0.0",
+    "depends": [
+        "report",
+    ],
+    "external_dependencies": {
+        "python": [
+            "unidecode",
+        ],
+    },
+    "author": "AvanzOSC, "
+              "Tecnativa, "
+              "Odoo Community Association (OCA), ",
+    "website": "https://github.com/OCA/server-tools",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "data": [
+        "wizard/report_duplicate_view.xml",
+        "views/report_xml_view.xml",
+    ],
+    'installable': True,
+}
