6666bf8ab71e83db8790bfad3e7441dc18787617
开阖软件 Jeff WANG <jeff@osbzr.com>
2018-03-15 12:08:17 +0800

    提升数据导入速度

---
+++ base_import_chunk/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Jarvis (www.odoomk.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Base import Chunk',
+    'description': """
+Base import Chunk
+""",
+    'depends': ['base_import'],
+    'category': 'Extra Tools',
+    'installable': True,
+    'auto_install': False,
+    'data': [
+    ],
+    'qweb': [
+    ],
+}
